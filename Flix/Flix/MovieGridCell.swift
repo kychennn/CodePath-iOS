a7581aa25c4fe3f999594ed80a06c59eeddf86e4
kychennn <kallychen2@gmail.com>
2022-03-02 23:00:39 -0500

    Flix Unit 2

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jiayi on 3/2/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
